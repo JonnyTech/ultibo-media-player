aea74781ab1748f9e505522be46d87032a079122
JonnyTech <JonnyTech@users.noreply.github.com>
2023-06-28 18:21:35 +0100

    Update upc.sh
    Tidy `rm` arguments

--- upc.sh
+++ upc.sh
@@ -2,6 +2,6 @@
 #./ffmpeg -i clip.mp4 -c:a pcm_s16le -ac 2 audio.wav
 #./ffmpeg -i clip.mp4 -map 0:v:0 -c:v libx264 video.h264 -map 0:a:0 -c:a pcm_s16le -ac 2 audio.wav
 
-rm -rf backup/ lib/ *.ppu *.o *.elf kern*.img *.lps
+rm -rf backup/ lib/ *.{o,ppu,rsj,lps,elf,img}
 ~/development/ultibo/fpc/bin/x86_64-linux/fpc.sh -Tultibo -Parm -CpARMV7A -WpRPI3B @RPI3.CFG -MObjFPC -Scghi -CX -O2 -XX -l -vewnhibq -Fu~/development/ultibo/* -otest "$@"
-rm -rf backup/ lib/ *.ppu *.o *.lps
+rm -rf backup/ lib/ *.{o,ppu,rsj,lps}
