dadb72d65f8658a4882474818864078d1f6991a4
JonnyTech <3584732+JonnyTech@users.noreply.github.com>
2023-10-22 11:59:17 +0400

    add addional qemu parameters

--- upc.sh
+++ upc.sh
@@ -3,4 +3,6 @@ rm -rf backup/ lib/ *.{o,ppu,s,rsj,lps,elf,img,bin}
 # ~/development/fpc/bin/x86_64-linux/ppcrossarm -n @upc.cfg -Tultibo -dQEMUVPB -WpQEMUVPB "$@"
 rm -rf backup/ lib/ *.{o,ppu,s,rsj,lps,elf}
 # NOTE: QUEMU does not support Pi GPU (omxplayer/vchiq)
-# qemu-system-arm -M versatilepb -cpu cortex-a8 -kernel kernel.bin -m 256M
+# qemu-system-arm -M versatilepb -cpu cortex-a8 -kernel kernel.bin -m 256M \
+# -monitor stdio -serial file:serial0.txt -drive file=disk.ima,if=sd,format=raw \
+# -net nic -net user,hostfwd=tcp::5080-:80,hostfwd=tcp::5023-:23 -usb
